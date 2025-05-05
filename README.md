98c7448c294cf141eb3b052a8774d38aefb6d2d1
ikreishi <vadas25@yandex.ru>
2025-05-05 15:41:40 +0500

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# Тестовое задание
+
+Приложение для вывода в таблицу списка пациентов
+
+# Стек технологий
+ - Java 21
+ - Spring Boot
+ - Postgres 
+ - Hibernate 
+ - ZK Framework
+
+# Сборка
+```bash
+mvn clean install
+```
---
+++ src/main/java/com/demomed/medpatient/MedPatientApplication.java
@@ -0,0 +1,13 @@
+package com.demomed.medpatient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedPatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedPatientApplication.class, args);
+	}
+
+}
